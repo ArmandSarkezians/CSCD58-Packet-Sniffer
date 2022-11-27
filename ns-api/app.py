2f79f815f51e64318ec9296429039a04c391f050
Ahmed Halat <ahmedhalat@gmail.com>
2022-11-27 13:07:38 -0500

    fix backend sniffer (#1)
    Co-authored-by: AhmedHalat <ahmedhalatgmail.com>

--- ns-api/app.py
+++ ns-api/app.py
@@ -1,8 +1,9 @@
-
-from time import sleep
+# pylint: disable=import-error
+import time, socket
 from flask import Flask
 from flask_cors import CORS
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+from structures import Ethernet, IPv4, TCP, UDP, ICMP
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +11,9 @@ CORS(app)
 app.logger.setLevel('INFO')
 
 app.debug = True
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*",  async_mode='threading')
+s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+thread = None
 
 @app.route('/')
 def index():
@@ -24,17 +27,49 @@ def arp():
 def whois():
     return 'Whois'
 
-# Pushing data to the client
-@app.route('/push')
-def push():
-    socketio.emit('packets', {'data': 'Hello World'})
-    return 'packets'
 
-@socketio.on('packets')
+@socketio.on('connect')
+def emit_market_data():
+    global thread
+    emit('packets', "sniffer")
+    if thread is None:
+        emit('packets', "start sniffer")
+        thread = socketio.start_background_task(target=sniffer)
+
+
+@socketio.event
 def sniffer():
-    app.logger.info('packets: ')
-    socketio.emit('packets', {'data': 'Hello World'})
+    print("Listening for packets...\n")
+    while True:
+        raw_data, addr = s.recvfrom(65536)
+        hdr = Ethernet(raw_data)
+        # Protocol 8 is for IPv4
+        if hdr.protocol == 8:
+            ipv4 = IPv4(raw_data)
 
-app.logger.info('Starting app')
+            # Protocol 1 is for ICMP
+            if ipv4.protocol == 1:
+                icmp = ICMP(ipv4.raw_data)
+                socketio.emit('packets', icmp)
+                print(icmp)
+            # Protocol 6 is for TCP
+            elif ipv4.protocol == 6:
+                tcp = TCP(ipv4.raw_data)
+                socketio.emit('packets', tcp)
+                print(tcp)
+            # Protocol 17 is for UDP
+            elif ipv4.protocol == 17:
+                udp = UDP(ipv4.raw_data)
+                socketio.emit('packets', udp)
+                print(udp)
+            # Other protocols
+            else:
+                print("Other protocol: ", ipv4.protocol)
+                socketio.emit('packets', ipv4.protocol)
+        # Other protocols
+        else:
+            print("Other protocol: ", hdr)
+            socketio.emit('packets', hdr)
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.logger.info('Starting app')
