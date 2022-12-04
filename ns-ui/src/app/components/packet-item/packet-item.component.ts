1bdaf23fa2e885163daffa0cc7440cb1da475645
Ahmed Halat <ahmedhalat@gmail.com>
2022-12-04 12:59:08 -0500

    parse packet data

--- ns-ui/src/app/components/packet-item/packet-item.component.ts
+++ ns-ui/src/app/components/packet-item/packet-item.component.ts
@@ -16,4 +16,22 @@ export class PacketItemComponent implements OnInit {
     this.packet = this.config.data.packet;
     console.log(this.packet);
   }
+
+  toHexString(buffer: any) {
+    const byteArray = new Uint8Array(buffer);
+    return byteArray.reduce((output, elem) =>
+      (output + ('0' + elem.toString(16)).slice(-2)),
+      '');
+  }
+
+  parseData(arrayBuffer: any) {
+    var hex = '';
+    var bytes = new Uint8Array(arrayBuffer);
+
+    bytes.forEach((byte) => {
+      hex += String.fromCharCode(byte);
+    });
+
+    return hex;
+  }
 }
